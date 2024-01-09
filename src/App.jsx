e0c3995bc9300436d5dec02e2a596300dda0dd36
Seba <sebase.pereyra@gmail.com>
2024-01-08 23:14:33 -0300

    Tie and reset game

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,14 @@ const TURNS = {
 }
 
 const WINNER_COMBOS = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
 ];
 
 
@@ -37,19 +37,22 @@ function App() {
   const [turn, setTurn] = useState(TURNS.X)
   const [winner, setWinner] = useState(null);
 
+  const checkTie = (boardToCheck) => {
+    return boardToCheck.every((square) => square !== null);
+  }
+
   const checkWinner = (boardToCheck) => {
-    for(const combo of WINNER_COMBOS){
-      const [a,b,c] = combo;
-      if(
-        boardToCheck[a] && 
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[b] === boardToCheck[c]
       )
-      return boardToCheck[a]; 
+        return boardToCheck[a];
     }
     return null;
   }
-
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
@@ -57,7 +60,7 @@ function App() {
     setWinner(null);
   }
   const updateBoard = (index) => {
-    if(board[index] || winner)
+    if (board[index] || winner)
       return;
 
     const newBoard = [...board];
@@ -67,20 +70,24 @@ function App() {
     const newTurn = (turn == TURNS.X ? TURNS.O : TURNS.X)
     setTurn(newTurn)
     const newWinner = checkWinner(newBoard);
-    if(newWinner){
+    if (newWinner) {
       setWinner(newWinner);
+    } else if(checkTie(newBoard)){
+      setWinner(false);
     }
+
   }
 
   return (
     <main className='board'>
       <h1>TA TE TI</h1>
+      <button onClick={resetGame}>Reset</button>
       <section className='game'>
         {
-          board.map((_, index) => {
+          board.map((sq, index) => {
             return (
               <Square key={index} index={index} updateBoard={updateBoard}>
-                {board[index]}
+                {sq}
               </Square>
             )
           })
@@ -90,24 +97,24 @@ function App() {
         <Square isSelected={turn == TURNS.X}> {TURNS.X} </Square>
         <Square isSelected={turn == TURNS.O}> {TURNS.O} </Square>
       </section>
-      
-      {winner !== null && (
-      <section className='winner'>
-        <div className='text'>
-          <h2>
-            {
-              winner === false ? 'It is a tie' : 'The winner is '
-            }
-          </h2>
-          <header className='win'>
-           { winner && <Square>{winner}</Square>}
-          </header>
-          <footer>
-            <button onClick={resetGame}>Start again</button>
-          </footer>
-        </div>
 
-      </section>
+      {winner !== null && (
+        <section className='winner'>
+          <div className='text'>
+            <h2>
+              {
+                winner === false ? 'It is a tie' : 'The winner is '
+              }
+            </h2>
+            <header className='win'>
+              {winner && <Square>{winner}</Square>}
+            </header> 
+            <footer>
+              <button onClick={resetGame}>Start again</button>
+            </footer>
+          </div>
+
+        </section>
       )}
     </main>
   )
